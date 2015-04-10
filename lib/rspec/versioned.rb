75bc8718d190fa31074e1d5eac7f58202449eb97
ddayal-fiksu <ddayal@fiksu.com>
2015-04-10 15:56:04 -0400

    working

---
+++ lib/rspec/versioned.rb
@@ -0,0 +1,97 @@
+require "rspec/versioned/version"
+require 'rspec/core'
+require 'rspec_ext/rspec_ext'
+require 'versioned_blocks'
+
+module RSpec
+  class Versioned
+    def self.apply
+      RSpec.configure do |config|
+        config.add_setting :notify_version_number, :default => true
+        config.add_setting :clear_lets_on_failure, :default => true
+
+        fetch_current_example = RSpec.respond_to?(:current_example) ?
+          proc { RSpec.current_example } : proc { |context| context.example }
+
+        config.around(:each) do |ex| # ex is a Procsy
+          notify_version_number = ex.metadata[:notify_version_number] || RSpec.configuration.notify_version_number
+
+          example = fetch_current_example.call(self)
+          example.new_version_info
+
+          if ex.metadata.has_key?(:versions)
+            if ex.metadata[:versions][:base_uri] || (VersionedBlocks.base_uri && VersionedBlocks.base_uri!='') # we have a URI
+              versioned_block(ex.metadata[:versions]) do |v, uri|
+                example.clear_exception
+                example.version.set(v, uri)
+                ex.run
+                RSpec.configuration.reporter.message("RSpec::Versioned testing /v#{v}") if notify_version_number
+              end
+            else # we don't have a URI
+              versioned_block(ex.metadata[:versions]) do |v|
+                example.clear_exception
+                example.version.set(v)
+                ex.run
+                RSpec.configuration.reporter.message("RSpec::Versioned testing /v#{v}") if notify_version_number
+              end
+            end
+          else # didn't specify any versions...just run the test!
+            ex.run
+          end
+          self.clear_lets if clear_lets
+        end
+      end
+    end
+  end
+end
+
+module HasVersionInfo
+  def new_version_info
+    @version_info = VersionInfoHolder.new
+  end
+
+  def version
+    @version_info
+  end
+
+  def clear_exception
+    @exception = nil
+  end
+end
+
+module RSpec
+  module Core
+    class Example
+      @version_info
+
+      include HasVersionInfo
+
+      class Procsy
+        @version_info
+
+        include HasVersionInfo
+      end
+    end
+  end
+end
+
+class VersionInfoHolder
+  attr_reader :number, :uri
+
+  def set(v_number, uri=nil)
+    @number = v_number
+    @uri = uri
+  end
+end
+
+module RSpec
+  module Core
+    class ExampleGroup
+      def clear_lets
+        @__memoized = {}
+      end
+    end
+  end
+end
+
+RSpec::Versioned.apply
---
+++ rspec-versioned.gemspec
@@ -0,0 +1,24 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'rspec/versioned/version'
+
+Gem::Specification.new do |spec|
+  spec.name          = "rspec-versioned"
+  spec.version       = Rspec::Versioned::VERSION
+  spec.authors       = ["devend711"]
+  spec.email         = ["devend711@gmail.com"]
+  spec.summary       = "Run tests over whatever API versions you want."
+  #spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.homepage      = ""
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files -z`.split("\x0")
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.6"
+  spec.add_development_dependency "rake"
+  spec.add_development_dependency "versioned_blocks"
+end
